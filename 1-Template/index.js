49946a61b69c21a588a023454c6e77c10a8b751a
vitaliy <nertis44@gmail.com>
2024-04-29 15:21:49 +0300

    1 - Проблемы JS

---
+++ 1-Template/index.js
@@ -0,0 +1,9 @@
+// js это язык с динамической типизацией, а это значит, вы можете выполнять любые операции с типами, например:
+console.log(typeof ('55' + 5)); // string
+console.log(typeof !!5); // boolean
+
+const student = {
+  name: 'John',
+  age: 20,
+};
+console.log(student.surname); // undefined
