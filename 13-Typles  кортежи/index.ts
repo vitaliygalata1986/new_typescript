fce5a8b911564257f6c382b8395330d08ca9243e
vitaliy <nertis44@gmail.com>
2024-05-09 22:32:45 +0300

    13-Typles

---
+++ 13-Typles  кортежи/index.ts
@@ -0,0 +1,26 @@
+// создаем кортеж
+const userDataTuple: [boolean, number, string] = [false, 20, 'John'];
+// userDataTuple[0] = 'true';
+
+// userDataTuple[3]
+// userDataTuple.push(50);
+// userDataTuple[3];
+const res = userDataTuple.map((t) => `${t} - data`);
+
+const userDataTuple2: [boolean, number, ...string[]] = [
+  true,
+  20,
+  'John',
+  'Alex',
+];
+// деструктуризируем
+const [isLogged, age, userName] = userDataTuple;
+
+const userDataTuple3: [...boolean[], number, string] = [
+  true,
+  false,
+  14,
+  'text',
+];
+
+// const newRes = userDataTuple2.map((t) => `${t.toUpperCase()} - data`);
