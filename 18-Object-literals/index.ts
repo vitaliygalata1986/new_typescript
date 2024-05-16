3ae27bc1c1aeb80b4774713359426fc892a9d171
vitaliy <nertis44@gmail.com>
2024-05-16 23:36:52 +0300

    Object Literals

---
+++ 18-Object-literals/index.ts
@@ -0,0 +1,15 @@
+const serverConfig: { protocol: 'http' | 'https'; port: 3000 | 3001 } = {
+  protocol: 'https',
+  port: 3001,
+};
+
+function startServer(
+  protocol: 'http' | 'https',
+  port: 3000 | 3001
+): 'Server started' {
+  console.log(`Server started on ${protocol}://server:${port}`);
+
+  return 'Server started';
+}
+
+startServer(serverConfig.protocol, serverConfig.port);
