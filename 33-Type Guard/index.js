5f3e64fffba4cb9e1ba0d5eb75d1839df6c53703
vitaliy <nertis44@gmail.com>
2024-06-15 23:57:26 +0300

    Type Guard

--- 33-Type Guard/index.js
+++ 33-Type Guard/index.js
@@ -1,7 +1,18 @@
-const num = new Number(5); // создадим сущность при пормощи конструктора new Number - таким образом, создадим цифру 5
-
-// объявим число как литерал
-const x = 500
-
-console.log(x==y)
-console.log(x===y)
+function printMsg(msg) {
+    // у нас будет входящий массив строк, числа или логические значения
+    if (Array.isArray(msg)) {
+        // isArray метод является ли входящий msg - массивом
+        msg.forEach(function (item) { return console.log(item); });
+    }
+    else if (typeof msg === 'number') {
+        console.log(msg.toFixed(2));
+    }
+    else {
+        console.log(msg);
+    }
+}
+printMsg(['ascas', 'asdasd']); // ascas  asdasd
+printMsg(10.888); // 10.89
+printMsg(true); // true
+var box = document.querySelector('.box');
+box === null || box === void 0 ? void 0 : box.addEventListener('click', function () { });
--- 33-Type Guard/index.ts
+++ 33-Type Guard/index.ts
@@ -1,63 +1,103 @@
-const num = new Number(5); // создадим сущность при пормощи конструктора new Number - таким образом, создадим цифру 5
-
-// объявим число как литерал
-const x = 5;
-
-console.log(x == num); // true
-console.log(x === num); // false
-
-let num2 = 5;
-
-let num1: Number = new Number(5);
-let num3: number = 5;
-
-num1 = num3; // num3 неявно преобразовалось в num1
-// num3 = num1
-
-// еще один вариант создания числа
-let num4 = Number(5);
-
-// преобразование типов
-const newNum = 5;
-const strNum: string = newNum.toString(); // преобразовали число в строку
-
-// обратная операция
-const str = '5';
-const numStr: number = +str; // используя унарный + - преобразовываем строку в число
-
-interface Department {
-  name: string;
-  budget: number;
-}
-
-const department: Department = {
-  name: 'web-dev',
-  budget: 50000,
-};
-
-// интерфейс будет описывать какой-то конкретный проект
-interface Project {
-  name: string; // имя отдела, который будет заниматься этим проектом
-  projectBudget: number;
-}
-
-// в коде динамически может формироваться еще один объект, который нужно сформировать на базе объекта department
 /*
-const mainProject: Project = {
-  ...department,
-  projectBudget: 5000,
-};
+function printMsg(msg: string[] | number | boolean): void {
+  // у нас будет входящий массив строк, числа или логические значения
+  if (Array.isArray(msg)) {
+    // isArray метод является ли входящий msg - массивом
+    msg.forEach((item) => console.log(item));
+  } else if (typeof msg === 'number') {
+    console.log(msg.toFixed(2));
+  } else {
+    console.log(msg);
+  }
+}
 */
 
-function tranformDepartment(department: Department, amount: number): Project {
-  // у нас могут приходить объекты, которые потходят под этот интерфейс
-  // amount - кол-во денег, которое закладывается под этот проект
-  // на выходе будем формировать новый объект, который будет потходить под интерфейс Project
-  return {
-    name: department.name,
-    projectBudget: amount,
+function printMsg(msg: string[] | number | boolean): void {
+  // у нас будет входящий массив строк, числа или логические значения
+  if (Array.isArray(msg)) {
+    // isArray метод является ли входящий msg - массивом
+    msg.forEach((item) => console.log(item));
+  } else if (isNumber(msg)) {
+    // используем пользовательский Type Guard
+    console.log(msg.toFixed(2));
+  } else {
+    console.log(msg);
+  }
+}
+printMsg(['ascas', 'asdasd']); // ascas  asdasd
+printMsg(10.888); // 10.89
+printMsg(true); // true
+
+/*
+const box = document.querySelector('.box');
+box?.addEventListener('click', () => {});
+*/
+
+// n: string[] | number | boolean - нам может приходить аргумент, который может быть несколькими типами
+// n is number - мы вернем то, что n - является числом (true/false)
+// это тоже самое, если бы написали: : boolean
+// n is number - такая запись чаще всего применяется в Type Guard
+// Функция inNumber вернет true только если аргумент будет числом.
+// Оператор is позволяет сказать, что будет возвращено логическое
+// значение, где проверяется , что n это число
+/*
+function isNumber(n: string[] | number | boolean): n is number {
+  return typeof n === 'number';
+}
+*/
+
+// упростим функцию, unknown - мы вообще можем не знать, что сюда может приходить
+function isNumber(n: unknown): n is number {
+  return typeof n === 'number';
+}
+interface Car {
+  engine: string;
+  wheels: {
+    number: number;
+    type: string;
   };
 }
 
-const mainProject: Project = tranformDepartment(department, 5000);
-console.log(mainProject); // { name: 'web-dev', projectBudget: 5000 }
+interface Ship {
+  engine: string;
+  sail: string;
+}
+
+// функция будет заниматься починкой какого-то транспортного средтсва
+function repairVehicle(vehicle: Car | Ship) {
+  // console.log(vehicle);
+  if (isCar(vehicle)) {
+    // если это автомобиль
+    console.log(vehicle.wheels.number); // то в таком случае мы имеем доступ до колес, тоесть мы будем иметь доступ к тем свойствам, которые есть в интерфейсе Car
+  } else if (isSip(vehicle)) {
+    console.log(vehicle.sail);
+  } else {
+    // код сюда некогда не дойдет
+    vehicle; // поэтому vehicle будет never
+  }
+}
+
+// typeguard
+function isCar(car: Car | Ship): car is Car {
+  // boolean здесь прописывать нельзя
+  // console.log(car); // { engine: 'audi', wheels: 4 }
+  // car должно относиться к интерфейсу Car
+  // чтобы проверить есть ли свойство в объекте - используем оператор in
+  // return 'wheels' in car;
+  return (car as Car).wheels.number !== undefined; // если мы утвердили car как автомобиль, то у него должно быть свойство wheels
+}
+
+function isSip(ship: Car | Ship): ship is Ship {
+  return 'sail' in ship;
+}
+
+const audi = {
+  engine: 'audi',
+  //wheels: 4,
+  wheels: {
+    number: 4,
+    type: 'string',
+  },
+};
+
+repairVehicle(audi); // 4
